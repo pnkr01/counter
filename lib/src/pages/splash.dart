5aeea5cf44c6fe927677b1ca17b04488fe9f150a
Pawan Kumar <pawankumar49871@gmail.com>
2023-04-09 17:00:01 +0530

    login

--- lib/src/pages/splash.dart
+++ lib/src/pages/splash.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:counter/src/global.dart';
+import 'package:counter/src/pages/home/home.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as https;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -23,16 +29,60 @@ class _SplashScreenState extends State<SplashScreen> {
 
   // }
 
+  setCookie(response) {
+    String rawCookie = response.headers['set-cookie']!;
+    int index = rawCookie.indexOf(';');
+    String refreshToken =
+        (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    int idx = refreshToken.indexOf("=");
+    if (kDebugMode) {
+      print(refreshToken.substring(idx + 1).trim());
+    }
+    String cookieID = refreshToken.substring(idx + 1).trim();
+    sharedPreferences.setString('cookie', cookieID);
+  }
+
+  tryLoggingThisUser() async {
+    var response = await https.post(
+        Uri.parse('http://115.240.101.71:8282/CampusPortalSOA/login'),
+        body: json.encode({
+          "username": regdNo.text,
+          "password": password.text,
+          "MemberType": "S"
+        }));
+    var decoded = jsonDecode(response.body);
+    print(decoded);
+    if (decoded["status"].toString().contains("success")) {
+      //cookie save
+      setCookie(response);
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const HomePage()),
+          (route) => false);
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(decoded["message"])));
+    } else {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(decoded["message"])));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Align(
+          child: SingleChildScrollView(
             child: Column(
               children: [
+                const SizedBox(
+                  height: 50,
+                ),
                 Container(
                   height: 70,
                   width: 70,
@@ -58,13 +108,21 @@ class _SplashScreenState extends State<SplashScreen> {
                 const SizedBox(
                   height: 10,
                 ),
-                const TextField(
-                  decoration: InputDecoration(
+                TextField(
+                  controller: regdNo,
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
                     hintText: "Enter Registraton Number",
                   ),
                 ),
-                const TextField(
-                  decoration: InputDecoration(
+                const SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  controller: password,
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
                     hintText: "Enter Password",
                   ),
                 ),
@@ -74,30 +132,47 @@ class _SplashScreenState extends State<SplashScreen> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      tryLoggingThisUser();
+                    },
                     child: const Text('Sign In'),
                   ),
                 ),
                 const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(width: 5),
-                    const Text('sign in with'),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
+                // Row(
+                //   children: [
+                //     Expanded(
+                //       child: Container(
+                //         height: 1,
+                //         color: Colors.grey,
+                //       ),
+                //     ),
+                //     const SizedBox(width: 5),
+                //     const Text('sign in with'),
+                //     const SizedBox(width: 5),
+                //     Expanded(
+                //       child: Container(
+                //         height: 1,
+                //         color: Colors.grey,
+                //       ),
+                //     ),
+                //   ],
+                // ),
+                // const SizedBox(height: 10),
+                // SizedBox(
+                //   width: double.infinity,
+                //   child: ElevatedButton(
+                //     onPressed: () {},
+                //     child: const Text('Google'),
+                //   ),
+                // ),
+                // SizedBox(
+                //   width: double.infinity,
+                //   child: ElevatedButton(
+                //     onPressed: () {},
+                //     child: const Text('Facebook'),
+                //   ),
+                // ),
               ],
             ),
           ),
