e6931728bd2ad9dfd05b3cb7df770df960a1acdd
Pawan Kumar <pawankumar49871@gmail.com>
2023-04-11 19:42:31 +0530

    tuesday:bye bye

--- lib/src/model/student_model.dart
+++ lib/src/model/student_model.dart
@@ -1,90 +1,36 @@
-import 'dart:convert';
-
 class Student {
-  String name;
-  String regdNo;
-  String dob;
-  String branch;
-  String sec;
-  String email;
+  String? name;
+  String? regdNo;
+  String? dob;
+  String? branch;
+  String? sec;
+  String? email;
+  String? mName;
+  String? phoneNo;
+  String? address;
+  String? pNo;
+  String? bloodgroup;
+  int? pincode;
+  String? caddress3;
+  String? parentNumber;
+  String? nationality;
+  String? maritalstatus;
   Student({
-    required this.name,
-    required this.regdNo,
-    required this.dob,
-    required this.branch,
-    required this.sec,
-    required this.email,
+    this.name,
+    this.regdNo,
+    this.dob,
+    this.branch,
+    this.sec,
+    this.email,
+    this.mName,
+    this.phoneNo,
+    this.address,
+    this.pNo,
+    this.bloodgroup,
+    this.pincode,
+    this.caddress3,
+    this.parentNumber,
+    this.nationality,
+    this.maritalstatus,
   });
-
-  Student copyWith({
-    String? name,
-    String? regdNo,
-    String? dob,
-    String? branch,
-    String? sec,
-    String? email,
-  }) {
-    return Student(
-      name: name ?? this.name,
-      regdNo: regdNo ?? this.regdNo,
-      dob: dob ?? this.dob,
-      branch: branch ?? this.branch,
-      sec: sec ?? this.sec,
-      email: email ?? this.email,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'regdNo': regdNo,
-      'dob': dob,
-      'branch': branch,
-      'sec': sec,
-      'email': email,
-    };
-  }
-
-  factory Student.fromMap(Map<String, dynamic> map) {
-    return Student(
-      name: map['name'] ?? '',
-      regdNo: map['enrollmentno'] ?? '',
-      dob: map['dateofbirth'] ?? '',
-      branch: map['branchdesc'] ?? '',
-      sec: map['sectioncode'] ?? '',
-      email: map['semailid'] ?? '',
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Student.fromJson(String source) => Student.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'Student(name: $name, regdNo: $regdNo, dob: $dob, branch: $branch, sec: $sec, email: $email)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-  
-    return other is Student &&
-      other.name == name &&
-      other.regdNo == regdNo &&
-      other.dob == dob &&
-      other.branch == branch &&
-      other.sec == sec &&
-      other.email == email;
-  }
-
-  @override
-  int get hashCode {
-    return name.hashCode ^
-      regdNo.hashCode ^
-      dob.hashCode ^
-      branch.hashCode ^
-      sec.hashCode ^
-      email.hashCode;
-  }
 }
--- lib/src/pages/home/home.dart
+++ lib/src/pages/home/home.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:counter/src/global.dart';
+import 'package:counter/src/model/student_model.dart';
+import 'package:counter/src/pages/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as https;
 
@@ -13,8 +16,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isLoading = true;
-  late String name;
-  late String regdNo;
 
   @override
   void initState() {
@@ -22,26 +23,76 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  Uint8List? list;
+  void requestImg() async {
+    //login this  user and get Cookie and then call image section.
+    try {
+      final https.Response response = await https.get(
+        Uri.parse(
+            'http://115.240.101.71:8282/CampusPortalSOA/image/studentPhoto'),
+        headers: {
+          'Cookie': 'JSESSIONID=${sharedPreferences.getString('cookie')}',
+        },
+      );
+      // print(profileImage);
+      setState(() {
+        list = response.bodyBytes;
+        print(list);
+      });
+    } catch (e) {}
+  }
+
+  late Student student;
+
   // late Student student;
 
   fetchStudentInfo() async {
-    try {
-      var response = await https.post(
-          Uri.parse('http://115.240.101.71:8282//CampusPortalSOA/studentinfo'),
-          headers: {
-            "Cookie": "JSESSIONID=${sharedPreferences.getString('cookie')}"
-          });
-      print(response.body);
-      var decoded = jsonDecode(response.body);
-      
-      setState(() {
-        name = decoded["detail"][0]["name"];
-        regdNo = decoded["detail"][0]["enrollmentno"];
-        isLoading = false;
-      });
-    } catch (e) {
-      print(e.toString());
+    //try {
+    var response = await https.post(
+        Uri.parse('http://115.240.101.71:8282//CampusPortalSOA/studentinfo'),
+        headers: {
+          "Cookie": "JSESSIONID=${sharedPreferences.getString('cookie')}"
+        });
+    print(response.body);
+    var decoded = jsonDecode(response.body);
+    if (decoded["detail"].isEmpty) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const SplashScreen()));
+    } else {
+      requestImg();
+      student = Student(
+        name: decoded["detail"][0]["name"] ?? "Not given",
+        regdNo: decoded["detail"][0]["enrollmentno"] ?? "Not given",
+        dob: decoded["detail"][0]["dateofbirth"] ?? "Not given",
+        branch: decoded["detail"][0]["branchdesc"] ?? "Not given",
+        sec: decoded["detail"][0]["sectioncode"] ?? "Not given",
+        email: decoded["detail"][0]["semailid"] ?? "Not given",
+        mName: decoded["detail"][0]["mothersname"] ?? "Not given",
+        address: decoded["detail"][0]["paddress2"] ?? "Not given",
+        bloodgroup: decoded["detail"][0]["bloodgroup"] ?? "Not given",
+        caddress3: decoded["detail"][0]["caddress2"] ?? "Not given",
+        maritalstatus: decoded["detail"][0]["maritalstatus"] ?? "Not given",
+        nationality: decoded["detail"][0]["nationality"] ?? "Not given",
+        pNo: decoded["detail"][0]["scellno"] ?? "Not given",
+        parentNumber: decoded["detail"][0]["stelephoneno"] ?? "Not given",
+        phoneNo: decoded["detail"][0]["scellno"] ?? "Not given",
+        pincode: decoded["detail"][0]["ppin"] ?? "Not given",
+      );
     }
+
+    setState(() {
+      isLoading = false;
+    });
+
+    // setState(() {
+    //   name = decoded["detail"][0]["name"];
+    //   regdNo = decoded["detail"][0]["enrollmentno"];
+    //   sec = decoded["detail"][0]["sectioncode"];
+    //   isLoading = false;
+    // });
+    // } catch (e) {
+    //   print(e.toString());
+    // }
   }
 
   @override
@@ -52,20 +103,170 @@ class _HomePageState extends State<HomePage> {
               child: CircularProgressIndicator(),
             )
           : SafeArea(
-              child: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  const Align(
-                    child: CircleAvatar(
-                      radius: 55,
-                      backgroundColor: Colors.red,
-                    ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      Align(
+                        child: CircleAvatar(
+                          radius: 55,
+                          backgroundColor: Colors.blue,
+                          child: CircleAvatar(
+                            radius: 52,
+                            backgroundImage: MemoryImage(list!),
+                          ),
+                        ),
+
+                        // ClipRRect(
+                        //   borderRadius: BorderRadius.circular(40),
+                        //   child: Image.memory(list!, width: 100),
+                        // ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 15),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.name!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.only(top: 10),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.regdNo!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.address!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.email!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.branch!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.sec!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.name!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.parentNumber!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.caddress3!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                          student.mName!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+                    ],
                   ),
-                  Text(name.toString()),
-                  Text(regdNo.toString()),
-                  // Text(student.email.toString()),
-                  // Text(student.dob.toString()),
-                ],
+                ),
               ),
             ),
     );
